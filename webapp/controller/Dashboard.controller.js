fb10f66e7409d11e10257766ae490153cf9f8d04
toledo3105 <toledovictor95@gmail.com>
2024-04-18 17:41:14 -0600

    18.04.2024

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,15 +1,43 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/vbm/AnalyticMap",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, AnalyticMap, JSONModel, Device) {
+    "use strict";
+    AnalyticMap.GeoJSONURL = "./map/Mexico.JSON";
+    return Controller.extend("geosales.geosales.controller.Dashboard", {
+      onInit: function () {
+        // var OData = {
+        //   Regions: [
+        //     {
+        //       country: "CDMX",
+        //       code: "MX-CDMX",
+        //       color: "rgb(27,126,172)",
+        //     },
+        //   ],
+        // };
 
-        return Controller.extend("geosales.geosales.controller.Dashboard", {
-            onInit: function () {
+        var oModel = new JSONModel("./data/states.json");
+        this.getView().setModel(oModel);
 
-            }
+        var oDeviceModel = new JSONModel(Device);
+        oDeviceModel.setDefaultBindingMode("OneWay");
+        this.getView().setModel(oDeviceModel, "device");
+
+        oModel.attachRequestCompleted(function (oEvent) {
+          var lvX = "X";
         });
+      },
+
+      formatCountry: function (oValue) {
+        var lv_v = oValue;
+      },
     });
+  }
+);
